87758d71fa88cd301c2d3f23568d955f557da087
Joshua Gutz <jgutz87@gmail.com>
2026-05-09 02:45:17 -0500

    Update build.gradle.kts to be  more forgiving regarding version numbers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,34 +1,20 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.my_android_app"
+    namespace = "com.example.my_android_app" // This should match your project name
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_android_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // minSdk = flutter.minSdkVersion
-        minSdk = 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode()
-        versionName = flutter.versionName()
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     signingConfigs {
@@ -40,13 +26,19 @@ android {
         }
     }
 
+    defaultConfig {
+        applicationId = "com.example.my_android_app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode()
+        versionName = flutter.versionName()
+    }
+
     buildTypes {
         getByName("release") {
-            // This replaces the "TODO" and the debug config
             signingConfig = signingConfigs.getByName("release")
         }
         getByName("debug") {
-            // We also sign debug builds so you can update without uninstalling
             signingConfig = signingConfigs.getByName("release")
         }
     }
